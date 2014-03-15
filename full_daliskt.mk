5c03535db333f953197e9835d6b16648fca08e4e
cholokei <leesl0416@naver.com>
2014-03-15 23:59:40 +0900

    daliskt: Initial import from dalikt
    Signed-off-by: cholokei <leesl0416@naver.com>

--- full_daliskt.mk
+++ full_daliskt.mk
@@ -31,12 +31,12 @@ BOARD_HAVE_NFC := true
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-# Inherit from dalikt device
-$(call inherit-product, device/samsung/dalikt/device.mk)
+# Inherit from daliskt device
+$(call inherit-product, device/samsung/daliskt/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_dalikt
-PRODUCT_DEVICE := dalikt
+PRODUCT_NAME := full_daliskt
+PRODUCT_DEVICE := daliskt
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := SHV-E120K
